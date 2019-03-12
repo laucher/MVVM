6b6a5cb574afbf20783f21fd30b81bafde4a76c2
adam <tcfz51@163.com>
2019-03-12 22:41:14 +0800

    增加编译的功能

--- js/compile.js
+++ js/compile.js
@@ -37,14 +37,24 @@ Compile.prototype = {
         })
     },
     compile(node){
-        
+        const nodeAttrs = node.attributes;
+
+        [].slice.call(nodeAttrs).forEach((attr)=>{
+            const attrName = attr.name;
+            if (Utils.isDirective(attrName)) {
+                const exp = attr.value;
+                const dir = attrName.substring(2);
+                if (Utils.isEventDirective(dir)) {
+                    Utils.eventHandler(node, me.$vm, exp, dir);
+                } else {
+                    Utils[dir] && Utils[dir](node, me.$vm, exp);
+                }
+                node.removeAttribute(attrName);
+            }
+        });
     },
     compileText(node,exp){
-        console.log(node)
-        node.textContent=this.vm[exp];
-        new Watcher(this.vm,exp,(nv,ov)=>{
-            node.textContent = nv;
-        })
+        compileUtil.text(node, this.vm, exp);
     }
 
 }
@@ -56,5 +66,20 @@ Utils ={
     },
     isTextNode(node){
         return node.nodeType === Node.TEXT_NODE;
-    }
+    },
+    isDirective(attr) {
+        return attr.indexOf('v-') == 0;
+    },
+    isEventDirective: function(dir) {
+        return dir.indexOf('on') === 0;
+    },
+    bind: function(node, vm, exp, dir) {
+        const updaterFn = updater[dir + 'Updater'];
+
+        updaterFn && updaterFn(node, this._getVMVal(vm, exp));
+
+        new Watcher(vm, exp, (value, oldValue)=>{
+            updaterFn && updaterFn(node, value, oldValue);
+        });
+    },
 }
